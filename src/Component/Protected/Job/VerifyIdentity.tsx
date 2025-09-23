2fb25501cea390cada6e38d79e8ff8ab444858d7
Festus <festusishioma123@gmail.com>
2025-09-23 21:37:14 +0100

    verify identity

---
+++ src/Component/Protected/Job/VerifyIdentity.tsx
@@ -0,0 +1,36 @@
+"use client";
+import Card from "@/Component/Card";
+import Image from "next/image";
+import Link from "next/link";
+export default function VerifyIdentity() {
+  return (
+    <Card className="flex flex-col items-center justify-center p-6 max-w-md mx-auto">
+      <h1 className="lg:text-2xl md:text-2xl text-l font-bold mb-4">Verify Your Identity</h1>
+      <p className="text-center mb-6">Build trust with onchain identity</p>
+
+      <Link href="https://app.polkaidentity.com/" target="_blank" className="flex items-center border p-3 rounded-lg mt-4 cursor-pointer">
+        <Image
+          src="/images/polkadot_identity.png"
+          alt="Verify Identity"
+          width={30}
+          height={30}
+        />
+
+        <h1 className="ml-3 lg:text-[15px] md:text-[15px] text-[12px]">Verify with Polka Identity</h1>
+      </Link>
+
+      <Link href="https://polkadot.polkassembly.io/" target="_blank" className="flex items-center mt-4 cursor-pointer">
+      <div className="flex items-center border p-3 rounded-lg mt-4 cursor-pointer">
+        <Image
+          src="/images/polkadot_assembly.png"
+          alt="Verify Identity"
+          width={30}
+          height={30}
+        />
+
+        <h1 className="ml-3 lg:text-[15px] md:text-[15px] text-[12px]">Verify with Polkassembly</h1>
+      </div>
+      </Link>
+    </Card>
+  );
+}
---
+++ src/app/dashboard/verify_identity/page.tsx
@@ -0,0 +1,5 @@
+import VerifyIdentity from "@/Component/Protected/Job/VerifyIdentity";
+
+export default function page() {
+  return <div className="mt-[10rem] px-[2rem]"><VerifyIdentity/></div>;
+}
